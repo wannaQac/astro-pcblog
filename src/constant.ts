7004b0c52be795ae042c52ab51ca118505f74269
宋辰轩 <1097180981@qq.com>
2024-09-14 15:55:05 +0800

    update desc

--- src/constant.ts
+++ src/constant.ts
@@ -10,7 +10,7 @@ export const description = '这是一个托管在 Cloudflare 上的个人博客�
 export const avatar = avatarImg
 
 export const quote =
-  '仰天大笑出门去，我辈岂是蓬蒿人'
+  '致力于分享电脑PC的相关知识，与大家一起成长。本站点基于Cloudflare托管，如果发现网页展示的不是最新内容，等待10s~30s左右即可，会弹出确认框是否强刷页面，或者使用快捷键 Ctrl + F5。'
 
 export const links = [
   { url: '/', title: '🌐首页' },
